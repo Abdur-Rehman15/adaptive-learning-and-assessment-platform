7e57e558ca0238cd6aa7fecf3cee4c1875be321f
Abdur-Rehman15 <abdur.rehman8872@gmail.com>
2026-07-22 14:23:47 +0500

    notification error corrected

--- backend/models/notification_model.py
+++ backend/models/notification_model.py
@@ -8,6 +8,6 @@ class Notification(NotificationBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     user_id: int | None = Field(default=None, foreign_key="user.id", ondelete="CASCADE")
     is_read: bool = Field(default=False)
-    created_at: datetime = Field(default_factory=datetime.now().date())
+    created_at: datetime = Field(default_factory=datetime.now)
     user: "User" = Relationship(back_populates="notifications")
 
